1e460740b04ee4d05bc090fafb40b0ddb54b0418
MehdyGhassem <125973079+MehdyGhassem@users.noreply.github.com>
2023-03-14 10:29:17 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let Avoiding = 0
 let LFSL = 0
 let LFSR = 0
 function soft_left () {
@@ -16,12 +17,32 @@ function soft_right () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
 }
+function avoidance_function () {
+    Avoiding = 1
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    basic.pause(100)
+    hard_right()
+    while (Avoiding == 1) {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+            soft_right()
+        } else {
+            soft_left()
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            Avoiding = 0
+            hard_right()
+        }
+    }
+}
 function hard_left () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
     basic.pause(100)
 }
 basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        avoidance_function()
+    }
     LFSL += maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     LFSR += maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     if (LFSL == 1 && LFSR == 1) {
